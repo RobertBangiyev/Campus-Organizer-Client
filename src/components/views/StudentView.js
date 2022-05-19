d0b7ab3c7309eb4b98501537c9cb1a2eadee1831
Robert Bangiyev <rbangiyev123@gmail.com>
2022-05-19 01:13:44 -0400

    clicking the campus when on student view now redirects to that campus page

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -4,6 +4,9 @@ StudentView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the single student view page.
 ================================================== */
+import { Link } from "react-router-dom";
+
+
 const StudentView = (props) => {
   const { student } = props;
 
@@ -12,7 +15,9 @@ const StudentView = (props) => {
     <div>
       <h1>{student.firstname + " " + student.lastname}</h1>
       <h3>{student.email}</h3>
-      {student.campus && <h3>{student.campus.name}</h3>}
+      {student.campus && <Link to={`/campus/${student.campusId}`}>
+            <h2>{student.campus.name}</h2>
+      </Link>}
       {!student.campus && <h3>Not enrolled in any colleges</h3>}
     </div>
   );
